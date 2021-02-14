e791b4e06302f4a0635c4a113a2a17a873045061
King of Chu <khai.phan.2007@gmail.com>
2021-02-14 22:38:18 +1100

    updating files

--- index.js
+++ index.js
@@ -1,74 +1,154 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
+const util = require('util');
 
-// * Your application should prompt the user for information like their name, location, bio, LinkedIn URL, and GitHub URL. Feel free to add any additional prompts you think of.
 
-inquirer
-  .prompt([
+const writeFileAsync = util.promisify(fs.writeFile);
+
+const promptUser = () => {
+  return inquirer.prompt([
+    {
+      type: 'confirm',
+      message: 'Before you begin, please ensure you have thoroughly prepared details of your project including all relevant information on installation, tests, links to the appropraite URLs',
+      name: 'checkpoint',
+    },
     {
       type: 'input',
-      message: 'What is the name of the project?',
+      message: 'What is your project name?',
       name: 'name',
     },
     {
       type: 'input',
-      message: 'Where are you located?',
-      name: 'location',
+      message: 'What is the motivation behind this project?',
+      name: 'motivation',
     },
     {
       type: 'input',
-      message: 'Write a bio.',
-      name: 'bio',
+      message: 'What are the key features of your application?',
+      name: 'features',
+    },
+    {
+      type: 'checkbox',
+    message: 'What technologies did you use?',
+    name: 'tech',
+    choices: ['JavaScript','CSS', 'HTML', 'Node.JS', 'Inquirer', 'Jest', 'BootStrap', 'JQuery']
+  },
+    {
+      type: 'input',
+      message: 'How to install this application?',
+      name: 'setup',
     },
     {
       type: 'input',
-      message: 'What is your LinkedIn URL?',
-      name: 'linkedin',
+      message: 'What tests need to be run to make sure everything is working as it should?',
+      name: 'tests',
+    },
+    {
+    type: 'input',
+    message: 'If you are open to contributions, what are your requirements for accepting them?',
+    name: 'contributions',
+    },
+    {
+      type: 'list',
+      message: 'What license arrangement is this project under?',
+      name: 'license',
+      choices: ['MIT','Apache', 'BSD']
     },
     {
       type: 'input',
-      message: 'What is your GitHub URL?',
+      message: 'Enter the filename and path to the project screenshot',
+      name: 'screenshot',
+    },
+    {
+      type: 'input',
+      message: 'Enter your project URL?',
+      name: 'projectUrl',
+    },
+    {
+      type: 'input',
+      message: 'Enter your GitHub URL?',
       name: 'github',
     },
-  ])
-  .then((response) => {
-    let data = `
-    
-    # ${name}
+    {
+      type: 'input',
+      message: 'Enter your email address',
+      name: 'email',
+    },
+  ]);
 
-      Foobar is a Python library for dealing with word pluralization.
+};
 
-      ## Installation
+const generateMarkdown = (response) =>
+`# ${response.name}
 
-      Use the package manager [pip](https://pip.pypa.io/en/stable/) to install foobar.
+[![License: ${response.license}](https://img.shields.io/badge/License-${response.license}-yellow.svg)](https://opensource.org/licenses/${response.license})
 
-      ```bash
-      pip install foobar
-      ```
+## Description
 
-      ## Usage
+#### Motivation
 
-      ```python
-      import foobar
+${response.motivation}
 
-      foobar.pluralize('word') # returns 'words'
-      foobar.pluralize('goose') # returns 'geese'
-      foobar.singularize('phenomena') # returns 'phenomenon'
-      ```
+#### Key features
 
-      ## Contributing
-      Pull requests are welcome. For major changes, please open an issue first to discuss what you would like to change.
+${response.features}
 
-      Please make sure to update tests as appropriate.
+#### Technologies used
 
-      ## License
-      [MIT](https://choosealicense.com/licenses/mit/)
-    
-    `
-    fs.writeFile('Readme.md', data, (err) =>
-        err ? console.error(err) : console.log('Success!')
-    );
-    }
-  );
+* ${response.tech}
 
+## Table of contents
+
+<!--ts-->
+* [Description](#Description)
+* [Installation](#Installation)
+* [Usage](#Usage)
+* [License](#License)
+* [Contributions](#Contributions)
+* [Test instructions](#Test-instructions)
+* [Questions](#Questions)
+<!--te-->
+
+## Installation
+
+${response.setup}
+
+## Usage
+
+![${response.name}](${response.screenshot})
+
+Below is the link to the project URL.
+
+Project URL: ${response.projectUrl}
+
+## Test instructions
+
+${response.tests}
+
+## License
+
+* ${response.license}
+
+## Contributions
+
+${response.contributions}
+
+## Questions
+
+If you have any questions, please contact me at:
+
+GitHub URL: ${response.github}
+
+Email: ${response.email}`
+
+  const init = () => {
+    promptUser()
+      .then((response) => writeFileAsync('Readme.md', generateMarkdown(response)))
+      .then(() => console.log('The file has been successfully created'))
+      .catch((err) => console.error(err));
+  };  
+
+  init()
+
+  
   
